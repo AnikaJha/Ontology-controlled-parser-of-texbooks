65932331ca6579708dbf5afd8e518204ae4976f4
Anika Dzhkha <64139165+AnikaJha@users.noreply.github.com>
2021-05-29 20:19:15 +0300

    Update morph.py

--- morph.py
+++ morph.py
@@ -1,36 +1,28 @@
 from razdel import sentenize, tokenize
 from navec import Navec
-from ipymarkup import show_dep_ascii_markup as show_markup
-from slovnet import Syntax, Morph
+from slovnet import Morph
 
-text = 'Для определения скорости течения воды в реку опущен поплавок, который за 50 с проходит 60 м между двумя вехами. Принимая скорость поплавка равной скорости течения, определите скорость течения воды.'
-chunk = []
-for sent in sentenize(text):
-    tokens = [_.text for _ in tokenize(sent.text)]
-    chunk.append(tokens)
-print(chunk[:2])
-
-navec1 = Navec.load('navec_news_v1_1B_250K_300d_100q.tar')
-morph = Morph.load('slovnet_morph_news_v1.tar', batch_size=4)
-morph.navec(navec1)
-
-markup = next(morph.map(chunk))
-
-words, deps = [], []
-for token in markup.tokens:
-    print(f'{token.text:>20} {token.tag}')
+# example of morph analysis
 
 
+def morph_analysis(text):
+    chunk = []
+    number_of_sents = 0
+    for sent in sentenize(text):
+        tokens = [_.text for _ in tokenize(sent.text)]
+        chunk.append(tokens)
+        number_of_sents += 1
 
+    navec1 = Navec.load('navec_news_v1_1B_250K_300d_100q.tar')
+    morph = Morph.load('slovnet_morph_news_v1.tar', batch_size=4)
+    morph.navec(navec1)
+    i = 0
 
+    while i < number_of_sents:
+        markup_m = next(morph.map(chunk[i:]))
+        i += 1
+        words, deps = [], []
 
+        for token in markup_m.tokens:
+            print(f'{token.text:>20} {token.tag}')
 
-
-
-
-
-
-
-dictionary_s = {'путь': 'километр' 'метр' 'сантиметр' 'длина' 'дорога' 'расстояние' 'км' 'см' 'дм' 'м'}
-dictionary_t = {'время': 'час' 'секунда' 'минута' 'ч' 'с' 'мин'}
-dictionary_v = {'скорость': 'м/с' 'км/ч' 'дм/мин' 'км/с' 'м/мин'}
