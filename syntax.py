498666163f9bd19de3292ac4f68c43628cfec676
Anika Dzhkha <64139165+AnikaJha@users.noreply.github.com>
2021-05-29 20:18:51 +0300

    Update syntax.py

--- syntax.py
+++ syntax.py
@@ -3,25 +3,34 @@ from navec import Navec
 from ipymarkup import show_dep_ascii_markup as show_markup
 from slovnet import Syntax, Morph
 
-text = 'Для определения скорости течения воды в реку опущен поплавок, который за 50 с проходит 60 м между двумя вехами. Принимая скорость поплавка равной скорости течения, определите скорость течения воды.'
-chunk = []
-for sent in sentenize(text):
-    tokens = [_.text for _ in tokenize(sent.text)]
-    chunk.append(tokens)
-print(chunk[:2])
 
-navec1 = Navec.load('navec_news_v1_1B_250K_300d_100q.tar')
-syntaxx = Syntax.load('slovnet_syntax_news_v1.tar')
-syntaxx.navec(navec1)
+# example of syntax analysis
 
-markup = next(syntaxx.map(chunk))
+def synt_analysis(text):
 
-words, deps = [], []
-for token in markup.tokens:
-    words.append(token.text)
-    source = int(token.head_id) - 1
-    target = int(token.id) - 1
-    if source > 0 and source != target:  # skip root, loops
-        deps.append([source, target, token.rel])
-show_markup(words, deps)
+    chunk = []
+    number_of_sents = 0
+    for sent in sentenize(text):
+        tokens = [_.text for _ in tokenize(sent.text)]
+        chunk.append(tokens)
+        number_of_sents += 1
+
+    navec1 = Navec.load('navec_news_v1_1B_250K_300d_100q.tar')
+    syntaxx = Syntax.load('slovnet_syntax_news_v1.tar')
+    syntaxx.navec(navec1)
+    i = 0
+
+    while i < number_of_sents:
+        markup2 = next(syntaxx.map(chunk[i:]))
+        i += 1
+        words, deps = [], []
+        for token in markup2.tokens:
+            words.append(token.text)
+            source = int(token.head_id) - 1
+            target = int(token.id) - 1
+
+            if source != target:  # skip root, loops
+                deps.append([source, target, token.rel])
+
+        show_markup(words, deps)
 
